e6e7c7257dea396d41f78f7c20fdbdcf45868f8b
Ksenobait_09@mail.ru <Ksenobait@eljur.ru>
2016-10-14 17:56:34 +0300

    matplotlib

--- vk_api.py
+++ vk_api.py
@@ -9,4 +9,8 @@ statistic = friends.execute()
 statistic = sorted(statistic.items(), key = lambda x:x[0])
 for i in statistic:
     print( '{} => {}'.format(i[0], i[1]))
-
+#arr = []
+#for i in statistic:
+#    arr.append(len(i[1]))
+#plt.hist(arr, 100)
+#plt.show()
